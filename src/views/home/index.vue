c2ca29eac64066f066b36df98fb049ff7de179a3
Vanisper <273266469@qq.com>
2024-09-04 02:05:05 +0800

    style: 💄 initial style

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,20 +3,22 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+    <div style="height: 100%;display: flex;flex-direction: column;align-items: center;padding: 30px 0 80px 0;">
+        <div style="display: flex;flex-wrap: wrap;justify-content: center;margin-bottom: auto;">
+            <a href="https://vitejs.dev" target="_blank">
+                <img src="/vite.svg" class="logo" alt="Vite logo" />
+            </a>
+            <a href="https://vuejs.org/" target="_blank">
+                <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
+            </a>
+        </div>
+        <HelloWorld msg="Vite + Vue" />
     </div>
-    <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped lang="less">
 .logo {
-    height: 6em;
+    height: 10em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
